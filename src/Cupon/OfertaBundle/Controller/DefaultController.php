bfafa55059d1ee6f434481c4a8b943f583781bdd
masolervalles <masolervalles@gmail.com>
2012-08-14 19:55:14 +0200

    Inicio del proyecto

---
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Cupon\OfertaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class DefaultController extends Controller
+{
+    
+    public function indexAction($name)
+    {
+        return $this->render('OfertaBundle:Default:index.html.twig', array('name' => $name));
+    }
+   
+    public function homeAction()
+    {
+	$name = "Miguel";
+	return $this->render('OfertaBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
